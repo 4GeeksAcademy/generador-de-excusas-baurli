d124886c95e6769361a17ac4e25125c06e71a023
Emosoul123 <baurlibennazar@gmail.com>
2025-12-26 04:33:54 +0000

    generador de excuas

--- src/app.js
+++ src/app.js
@@ -7,5 +7,33 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ['The dog', 'My grandma', 'His turtle', 'My bird'];
+  let action = ['ate', 'peed on', 'crushed', 'broke'];
+  let what = ['my homework', 'the keys', 'the car', 'my phone'];
+  let when = ['before class', 'right on time', 'when I was sleeping', 'during lunch'];
+
+function generateExcuse() {
+  let randomWho = who[Math.floor(Math.random() * who.length)];
+  let randomAction = action[Math.floor(Math.random() * action.length)];
+  let randomWhat = what[Math.floor(Math.random() * what.length)];
+  let randomWhen = when[Math.floor(Math.random() * when.length)];
+
+    return randomWho + " " + randomAction + " " + randomWhat + " " + randomWhen;
+}
+
+  const excuseEl = document.querySelector("#excuse");
+  const btn = document.querySelector("#btn");
+
+  excuseEl.innerHTML = generateExcuse();
+
+btn.addEventListener("click", function () {
+  excuseEl.style.opacity = 0;
+  setTimeout(() => {
+    excuseEl.innerHTML = generateExcuse();
+    excuseEl.style.opacity = 1;
+  }, 150);
+});
+
 };
+
+
